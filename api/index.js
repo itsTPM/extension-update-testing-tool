3304ea9a5682cfce1b7fbaef91c564143dd2b5e9
itsTPM <26815728+itsTPM@users.noreply.github.com>
2023-12-07 11:43:54 +0300

    move unzip to index.js & del zip.js

--- api/index.js
+++ api/index.js
@@ -1,36 +1,16 @@
-// Copyright 2023 Google LLC
-
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-
-//     https://www.apache.org/licenses/LICENSE-2.0
-
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 const state = require("./state");
 const utils = require("./utils");
+const { mkdir, readFile, writeFile } = require("fs/promises");
+const path = require("path");
+const AdmZip = require("adm-zip");
 
 const express = require("express");
 
 const app = express();
 
-// Serve public directory on root (HTML, JS files etc.)
 app.use("/", express.static("public"));
 
-// Handle zip based uploads automatically
-
 app.get("/status", require("./routes/status"));
-
-app.post(
-  "/upload/zip",
-  require("./utils").setupTmpDirectory,
-  require("./routes/upload/zip")
-);
 
 app.get(
   "/extension.crx",
@@ -52,3 +32,23 @@ app.get(
 
 app.listen(state.PORT);
 console.log(`Listening at http://localhost:${state.PORT}...`);
+
+async function handleZip() {
+  const filePath = path.join("C:/ext.zip");
+  const file = await readFile(filePath);
+  
+  await writeFile("tmp/extension.zip", file);
+
+  const zip = new AdmZip("tmp/extension.zip");
+
+  await mkdir("tmp/unpacked", { recursive: true });
+
+  await new Promise((resolve, reject) =>
+    zip.extractAllToAsync("tmp/unpacked", true, false, (err) => {
+      if (err) reject(err);
+      resolve(undefined);
+    })
+  );
+}
+
+handleZip();
--- public/index.js
+++ public/index.js
@@ -2,31 +2,6 @@ const STATUS_ELEMENT = document.getElementById("status");
 const WINDOWS_POLICY_ID = document.getElementById("windows-policy-id");
 const WINDOWS_POLICY_HOST = document.getElementById("windows-policy-host");
 
-let uploaded = false;
-
-async function onUpload() {
-  STATUS_ELEMENT.innerText = "Loading...";
-
-  fetch("/upload/zip", { method: 'POST' }).then(onUploadFinished);
-}
-
-async function onUploadFinished(response) {
-  const data = await response.json();
-
-  if (data.success) {
-    showDownloadInstructions(data.id, data.name, data.version);
-  } else {
-    STATUS_ELEMENT.innerText = data.message;
-  }
-}
-
-function showDownloadInstructions(id, name, version) {
-  STATUS_ELEMENT.innerText = `Serving "${name}" version ${version}`;
-
-  WINDOWS_POLICY_ID.innerText = id;
-  WINDOWS_POLICY_HOST.innerText = window.location.origin;
-}
-
 fetch("/status")
   .then((response) => response.json())
   .then((response) => {
@@ -37,4 +12,9 @@ fetch("/status")
     }
   });
 
-onUpload();
+function showDownloadInstructions(id, name, version) {
+  STATUS_ELEMENT.innerText = `Serving "${name}" version ${version}`;
+
+  WINDOWS_POLICY_ID.innerText = id;
+  WINDOWS_POLICY_HOST.innerText = window.location.origin;
+}
